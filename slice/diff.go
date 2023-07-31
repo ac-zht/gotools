e29f0cee4ed792965fb6b734cee616df03a0da1a
zht <2032754457@qq.com>
2023-07-31 23:02:15 +0800

    切片-差集

--- slice/diff.go
+++ slice/diff.go
@@ -1,8 +1,32 @@
 package slice
 
-func Diff[T comparable](s1 []T, s2 []T) []T {
-    m1 := Map[T](s1)
-    for _, v := range s2 {
-        delete(m1, v)
-    }
+func Diff[T comparable](src, dst []T) []T {
+	srcMap := Map[T](src)
+	for _, v := range dst {
+		delete(srcMap, v)
+	}
+	diff := make([]T, 0, len(srcMap))
+	for k, _ := range srcMap {
+		diff = append(diff, k)
+	}
+	return diff
+}
+
+func SymmetricDiff[T comparable](src, dst []T) []T {
+	srcMap := Map[T](src)
+	dstMap := Map[T](dst)
+	for _, v := range dst {
+		if _, ok := srcMap[v]; ok {
+			delete(srcMap, v)
+			delete(dstMap, v)
+		}
+	}
+	for k, _ := range dstMap {
+		srcMap[k] = struct{}{}
+	}
+	sd := make([]T, 0, len(srcMap))
+	for k, _ := range srcMap {
+		sd = append(sd, k)
+	}
+	return sd
 }
--- slice/intersection_test.go
+++ slice/intersection_test.go
@@ -5,37 +5,37 @@ import (
 	"testing"
 )
 
-func TestDiff(t *testing.T) {
-    testCase := []struct {
-        name string
-        src  []int
-        dst  []int
-        want []int
-    }{
-        {
-            name: "exist intersection",
-            src:  []int{1, 2},
-            dst:  []int{2, 3, 4},
-            want: []int{2},
-        },
-        {
-            name: "contain",
-            src:  []int{1, 2},
-            dst:  []int{1, 2, 3},
-            want: []int{1, 2},
-        },
-        {
-            name: "no intersection",
-            src:  []int{1, 2},
-            dst:  []int{3, 4},
-            want: []int{},
-        },
-    }
+func TestIntersection(t *testing.T) {
+	testCase := []struct {
+		name string
+		src  []int
+		dst  []int
+		want []int
+	}{
+		{
+			name: "exist intersection",
+			src:  []int{1, 2},
+			dst:  []int{2, 3, 4},
+			want: []int{2},
+		},
+		{
+			name: "contain",
+			src:  []int{1, 2},
+			dst:  []int{1, 2, 3},
+			want: []int{1, 2},
+		},
+		{
+			name: "no intersection",
+			src:  []int{1, 2},
+			dst:  []int{3, 4},
+			want: []int{},
+		},
+	}
 
-    for _, tc := range testCase {
-        t.Run(tc.name, func(t *testing.T) {
-            d := Intersection[int](tc.src, tc.dst)
-            assert.Equal(t, tc.want, d)
-        })
-    }
+	for _, tc := range testCase {
+		t.Run(tc.name, func(t *testing.T) {
+			intersect := Intersection[int](tc.src, tc.dst)
+			assert.Equal(t, tc.want, intersect)
+		})
+	}
 }
